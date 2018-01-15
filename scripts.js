9732bfc972ece3801d76df8366418f3940ec1d6a
aleksandra919 <aleksandrarolka91@gmail.com>
2018-01-15 10:57:04 +0100

    add

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+const result = [];
+
+const lotto = function() {
+    if(result.length === 6) return;
+    const lottoResult = Math.floor(Math.random() * 49 + 1);
+ 
+
+    for (let i = 0; i < result.length; i++) {
+        if(lottoResult === result[i]) {
+            return lotto();
+        }
+    }
+    const div = document.createElement("div");
+    div.textContent = lottoResult;
+    result.push(lottoResult);
+    document.body.appendChild(div);
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", lotto);
